62f445a6107c7f26942e3040ce8c601824250abf
Yu Hao Ye <rxye1991@gmail.com>
2019-11-20 18:54:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
 # Add your code here
+
+class Dog
+  @@all = [ ]
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def self.all
+    @@all 
+  end 
+
+  def self.clear_all
+    @@all.clear
+  end 
+  
+  def self.print_all
+    @@all 
+  end 
+  
+end  # End of class 
+
